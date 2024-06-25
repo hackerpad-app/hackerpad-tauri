126fda1446443fbbf33ded830335ce275a435518
Miko <mikolajkacki98@gmail.com>
2024-06-25 12:13:50 +0100

    Database works on prod.

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -2,6 +2,9 @@ use sqlite::{Connection, State};
 use tauri::command;
 use serde::{Serialize, Deserialize};
 
+use tauri::api::path::data_dir; // Make sure to import data_dir
+use std::path::PathBuf;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     id: i64,
@@ -11,7 +14,9 @@ pub struct User {
 
 #[command]
 pub fn initialize_db() -> Result<String, String> {
-    let connection = Connection::open("app.db").map_err(|e| e.to_string())?;
+    let db_path: PathBuf = data_dir().unwrap_or_else(|| PathBuf::from(".")).join("com.hackerpad-dev.dev/app.db");
+    let connection = Connection::open(db_path).map_err(|e| e.to_string())?;
+
     connection
         .execute(
             "
@@ -27,7 +32,9 @@ pub fn initialize_db() -> Result<String, String> {
 
 #[command]
 pub fn add_user(name: String) -> Result<String, String> {
-    let connection = Connection::open("app.db").map_err(|e| e.to_string())?;
+    let db_path: PathBuf = data_dir().unwrap_or_else(|| PathBuf::from(".")).join("com.hackerpad-dev.dev/app.db");
+    let connection = Connection::open(db_path).map_err(|e| e.to_string())?;
+    
     let mut statement = connection
         .prepare("INSERT INTO users (name) VALUES (?)")
         .map_err(|e| e.to_string())?;
@@ -38,7 +45,9 @@ pub fn add_user(name: String) -> Result<String, String> {
 
 #[command]
 pub fn remove_user(id: i64) -> Result<String, String> {
-    let connection = Connection::open("app.db").map_err(|e| e.to_string())?;
+    let db_path: PathBuf = data_dir().unwrap_or_else(|| PathBuf::from(".")).join("com.hackerpad-dev.dev/app.db");
+    let connection = Connection::open(db_path).map_err(|e| e.to_string())?;
+
     let mut statement = connection
         .prepare("DELETE FROM users WHERE id = ?")
         .map_err(|e| e.to_string())?;
@@ -49,7 +58,9 @@ pub fn remove_user(id: i64) -> Result<String, String> {
 
 #[command]
 pub fn get_users() -> Result<Vec<User>, String> {
-    let connection = Connection::open("app.db").map_err(|e| e.to_string())?;
+    let db_path: PathBuf = data_dir().unwrap_or_else(|| PathBuf::from(".")).join("com.hackerpad-dev.dev/app.db");
+    let connection = Connection::open(db_path).map_err(|e| e.to_string())?;
+
     let mut statement = connection
         .prepare("SELECT id, name FROM users")
         .map_err(|e| e.to_string())?;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,3 +11,4 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+  
