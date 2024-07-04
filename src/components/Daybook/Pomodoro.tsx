bee3817629b15f2be24b1d5e35dd34867473f93e
Miko <mikolajkacki98@gmail.com>
2024-07-04 12:50:45 +0100

    NoteList implemented, rough Pomodoro added.

---
+++ src/components/Daybook/Pomodoro.tsx
@@ -0,0 +1,86 @@
+import { useState, useEffect, useRef } from "react";
+
+export const config = {
+  session_time: 50,
+  break_time: 10,
+};
+
+const SessionTimer = () => {
+  const [minutes, setMinutes] = useState(config.session_time);
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef<number | null>(null); 
+
+  const startTimer = () => {
+    if (intervalRef.current !== null) return; // if timer is already running, do nothing
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds === 0) {
+          setMinutes((prevMinutes) => {
+            if (prevMinutes !== 0) {
+              return prevMinutes - 1;
+            } else {
+              const audio = new Audio("/public/completed.mp3");
+              audio.play();
+              return config.session_time; // use config.session_time instead of hard-coded value
+            }
+          });
+          return 59;
+        } else {
+          return prevSeconds - 1;
+        }
+      });
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    if (intervalRef.current === null) return; // if timer is not running, do nothing
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    if (intervalRef.current !== null) clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setMinutes(config.session_time); // use config.session_time instead of hard-coded value
+    setSeconds(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      // cleanup function to clear interval on component unmount
+      if (intervalRef.current !== null) clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return (
+    <div className="timer">
+      <div className="timer-tools">
+        <button
+          className="start-timer"
+          onClick={startTimer}
+          disabled={isRunning}
+        ></button>
+        <button
+          className="stop-timer"
+          onClick={stopTimer}
+          disabled={!isRunning}
+        ></button>
+        <button className="reset-timer" onClick={resetTimer}></button>
+      </div>
+      <div className="clock">
+        <p>
+          {timerMinutes}:{timerSeconds}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SessionTimer;
--- src/components/Daybook/Sidebar.tsx
+++ src/components/Daybook/Sidebar.tsx
@@ -1,9 +1,68 @@
 import Note from "./../../types/Note";
+import SessionTimer from "./Pomodoro";
 
 interface SidebarProps {
   searchResults: Note[];
   allNotes: Note[];
   setCurrentNote: React.Dispatch<React.SetStateAction<Note>>;
+}
+
+interface NoteListProps {
+  searchResults: Note[];
+  allNotes: Note[];
+  setCurrentNote: React.Dispatch<React.SetStateAction<Note>>;
+}
+
+interface NoteItemProps {
+  note: Note;
+  setCurrentNote: React.Dispatch<React.SetStateAction<Note>>;
+}
+
+const NoteItem = ({ note, setCurrentNote }: NoteItemProps) => {
+  return (
+    <div
+      className="cursor-pointer p-2 flex flex-row w-full bg-transparent border border-transparent hover:border-bright-green transition-colors duration-100 justify-between items-center rounded-lg"
+      onClick={() => setCurrentNote(note)}
+    >
+      <div className="p-2">
+        <div className="text-sm font-bold">{note.headline}</div>
+        <div className="text-sm">
+          {note.content.split(" ").slice(0, 4).join(" ") + "..."}
+        </div>
+      </div>
+      <div className="text-xs text-center p-2 text-white opacity-25  self-start  flex-shrink-0">
+        {new Date(note.created_at).toLocaleDateString()}
+      </div>
+    </div>
+  );
+};
+
+function NoteList({ searchResults, allNotes, setCurrentNote }: NoteListProps) {
+  return (
+    <div
+      style={{
+        borderColor: "rgba(22, 163, 74, 0.5)",
+        borderWidth: "2px",
+        borderStyle: "solid",
+      }}
+      className="h-4/5 bg-transparent rounded-lg"
+    >
+      <div className="flex-none w-full h-full items-cente bg-transparent hover:border-dark-green">
+        {(searchResults?.length > 0
+          ? searchResults
+          : allNotes.length > 0
+          ? allNotes.sort(
+              (a, b) =>
+                new Date(b.created_at).getTime() -
+                new Date(a.created_at).getTime()
+            )
+          : []
+        ).map((note: Note, index) => (
+          <NoteItem key={index} note={note} setCurrentNote={setCurrentNote} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default function Sidebar({
@@ -12,33 +71,14 @@ export default function Sidebar({
   setCurrentNote,
 }: SidebarProps) {
   return (
-    <div className="w-1/4 h-screen bg-[#af9191] flex flex-col justify-center items-center border-2 border-[#2e0c0c]">
-      <div className="w-full h-4/5 flex flex-col bg-black">
-        <div className="flex-none w-full h-full pt-4 items-center">
-          {(searchResults?.length > 0
-            ? searchResults
-            : allNotes.length > 0
-            ? allNotes.sort(
-                (a, b) =>
-                  new Date(b.created_at).getTime() -
-                  new Date(a.created_at).getTime()
-              )
-            : []
-          ).map((note: Note, index) => (
-            <div
-              key={index}
-              className="cursor-pointer flex flex-col w-full bg-transparent hover:bg-red-500 transition-colors duration-300 justify-between items-center rounded-lg"
-              onClick={() => setCurrentNote(note)}
-            >
-              <div className="text-lg font-bold text-center">
-                {note.headline}
-              </div>
-              <div className="text-sm text-center">
-                {note.content.split(" ").slice(0, 4).join(" ") + "..."}
-              </div>
-            </div>
-          ))}
-        </div>
+    <div className="h-screen bg-dark-green flex flex-col justify-center items-center">
+      <div className="w-full h-screen flex-col bg-dark-green p-5 mt-10">
+        <NoteList
+          searchResults={searchResults}
+          allNotes={allNotes}
+          setCurrentNote={setCurrentNote}
+        />
+        <SessionTimer />
       </div>
     </div>
   );
