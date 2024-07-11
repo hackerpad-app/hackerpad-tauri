fabc5efebc8facf5bb7bf10ec0d679974719ab06
Miko <mikolajkacki98@gmail.com>
2024-07-11 12:09:20 +0100

    CSS of editor is improved.

--- src/components/Daybook/Editor.tsx
+++ src/components/Daybook/Editor.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { PiNotePencilLight } from "react-icons/pi";
 import { AiOutlineDelete } from "react-icons/ai";
+
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import Confetti from "react-dom-confetti";
 
 import Note from "../../types/Note";
 
@@ -53,12 +57,12 @@ const Tools = ({
     <div className="flex justify-between">
       <div className="flex ml-2">
         <button onClick={handleCreateNote} className="mr-3 bg-transparent">
-          <div className="py-4 text-bright-green" style={{ fontSize: "25px" }}>
+          <div className="py-4 text-bright-green" style={{ fontSize: "2rem" }}>
             <PiNotePencilLight />
           </div>
         </button>
         <button onClick={removeNote} className="bg-transparent">
-          <div className="py-4 text-bright-green" style={{ fontSize: "25px" }}>
+          <div className="py-4 text-bright-green" style={{ fontSize: "2rem" }}>
             <AiOutlineDelete />
           </div>
         </button>
@@ -83,12 +87,15 @@ export default function Editor({
   updateNote,
   setSearchQuery,
 }: EditorProps) {
+  const [confetti, setConfetti] = useState(false);
+  const [checkedCount, setCheckedCount] = useState(0);
+
   const headlineEditor = useEditor({
     extensions: [StarterKit, Highlight, Typography],
     content: "",
     editorProps: {
       attributes: {
-        class: "prose",
+        class: "prose max-w-none h-full w-full",
       },
     },
     onUpdate: () => {
@@ -102,11 +109,11 @@ export default function Editor({
   });
 
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Typography],
+    extensions: [StarterKit, TaskList, TaskItem, Highlight, Typography],
     content: ``,
     editorProps: {
       attributes: {
-        class: "prose",
+        class: "prose max-w-none h-1/2 w-full",
       },
     },
     onUpdate: () => {
@@ -114,6 +121,14 @@ export default function Editor({
       if (newContent !== undefined && displayedNote) {
         const newNote = { ...displayedNote, content: newContent };
         setDisplayedNote(newNote);
+        let newCheckedCount = (
+          newContent.match(/<input type="checkbox" checked="checked">/g) || []
+        ).length;
+        if (newCheckedCount > checkedCount) {
+          setConfetti(true);
+          setTimeout(() => setConfetti(false), 100);
+        }
+        setCheckedCount(newCheckedCount);
       }
     },
   });
@@ -132,7 +147,7 @@ export default function Editor({
 
   return (
     <div className="bg-dark-green relative h-screen w-full">
-      <div className="relative h-2/30 ">
+      <div className="relative ">
         <Tools
           createNote={createNote}
           removeNote={removeNote}
@@ -141,11 +156,12 @@ export default function Editor({
           setSearchQuery={setSearchQuery}
         />
       </div>
-      <div className="relative h-1/10 border border-green-900">
+      <div className="headline relative border border-green-900">
         <EditorContent editor={headlineEditor} />
       </div>
-      <div className="relative h-full border border-green-900 overflow-y-auto">
+      <div className="h-full w-full border border-green-900 overflow-y-auto">
         <EditorContent editor={editor} />
+        <Confetti active={confetti} />
       </div>
     </div>
   );
